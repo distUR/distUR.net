540ead5f153da0828b618a9a0889c8cadc62ba9f
unbornchikken <gabor.mezo@outlook.com>
2015-04-17 16:35:56 +0200

    connect accounts proof of concept

--- distUR.net.js
+++ distUR.net.js
@@ -1,23 +1,48 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.body.events({
+        "click .c-gh": function () {
+            if (Meteor.user()) {
+                Meteor.connectWith("github");
+            }
+        },
+        "click .c-db": function () {
+            if (Meteor.user()) {
+                Meteor.connectWith(Package["gcampax:dropbox-oauth"]["DropboxOAuth"]);
+            }
+        }
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    // counter starts at 0
+    Session.setDefault("counter", 0);
+
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get("counter");
+        }
+    });
+
+    Template.hello.events({
+        "click button": function () {
+            // increment the counter when button is clicked
+            Session.set("counter", Session.get("counter") + 1);
+        }
+    });
+
+    Meteor.connectWith = function (service, options, callback) {
+        // Support a callback without options
+        if (!callback && typeof options === "function") {
+            callback = options;
+            options = null;
+        }
+        var connectCredentialRequestCompleteCallback = Accounts.oauth.connectCredentialRequestCompleteHandler(callback);
+        var Service = typeof service === "string" ? Package[service][makePascalCased(service)] : service;
+        Service.requestCredential(options, connectCredentialRequestCompleteCallback);
+    };
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
