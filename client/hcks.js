69405c0952576c47cf5e02567fbda4a698773f9f
unbornchikken <gabor.mezo@outlook.com>
2015-04-17 22:21:13 +0200

    template added

---
+++ client/hcks.js
@@ -0,0 +1,11 @@
+// Connector plugin patch:
+Meteor.connectWith = function (service, options, callback) {
+    // Support a callback without options
+    if (!callback && typeof options === "function") {
+        callback = options;
+        options = null;
+    }
+    var connectCredentialRequestCompleteCallback = Accounts.oauth.connectCredentialRequestCompleteHandler(callback);
+    var Service = typeof service === "string" ? Package[service][makePascalCased(service)] : service;
+    Service.requestCredential(options, connectCredentialRequestCompleteCallback);
+};
