4689c09ed24996bb8981cb6d1ee46899d03d9eba
unbornchikken <gabor.mezo@outlook.com>
2015-05-08 13:30:32 +0200

    updated connect package

--- client/_init.js
+++ client/_init.js
@@ -2,19 +2,4 @@
 
 /* global Meteor,Package,Accounts,Promise,distUR */
 
-// Connector plugin patch:
-Meteor.connectWith = function (service, options, callback) {
-    // Support a callback without options
-    if (!callback && typeof options === "function") {
-        callback = options;
-        options = null;
-    }
-    var makePascalCased = function (word) {
-        return word[0].toUpperCase() + word.slice(1).toLowerCase();
-    };
-    var connectCredentialRequestCompleteCallback = Accounts.oauth.connectCredentialRequestCompleteHandler(callback);
-    var Service = typeof service === "string" ? Package[service][makePascalCased(service)] : service;
-    Service.requestCredential(options, connectCredentialRequestCompleteCallback);
-};
-
 distUR.call = Promise.promisify(Meteor.call, Meteor);
